1f42c4a4fa4b41e77abe750ff86f8e6c7d76b232
Алексей Бормотов <92627133+Bormoleshka@users.noreply.github.com>
2024-03-19 15:07:33 +0300

    Update HwTest.java

--- src/test/java/HwTest.java
+++ src/test/java/HwTest.java
@@ -10,23 +10,108 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 public class HwTest {
-    private AndroidDriver driver;
-    private DriverFactory driverFactory;
+    private final DriverFactory driverFactory = DriverFactory();
+    private AnfroidDriver<?> drriver;
+    
+    @AndroidFindBy(accessibility = "Каталог")
+    MobileElement catalogElement;
+    
+    @AndroidFindBy(id = "block_search_widget")
+    MobileElement searchWidget;
+    
+    @AndroidFindBy(id = "search_src_text")
+    MobileElement searchText;
+    
+    @AndroidFindBy(uiAutomator = "resourceIdMatches(\".*id/title\").textContains(\"товаров\")")
+    MobileElement foundTitle;
+    
+    @AndroidFindBy(id = "filter_label")
+    MobileElement filterLabel;
+    
+    @AndroidFindBy(id = "filter_switch")
+    MobileElement filterSwitch;
+    
+    @AndroidFindBy(id = "apply_filters")
+    MobileElement applyFiltersButton;
+    
+    @AndroidFindBy(id = "specify_category_pager")
+    MobileElement specifyCategoryPager;
 
     @Before
+    @Step("Создание драйвера")
     public void setDriver() {
-        driverFactory = new DriverFactory();
+    
         driver = driverFactory.setUp();
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this)
     }
 
-    @org.junit.Test
+   
     @Test
-    public void test() throws InterruptedException {
-        Thread.sleep(Long.parseLong("5000"));
+    @Step("Тест Мвидео")
+    public void test() {
+        Assert.assertFalse(catalogElement.isSelected());
+        catalogElement.click();
+        swipe(Direction.UP);
+        swipe(Direction.DOWN);
+        searchWidget.click();
+        String television = "Телевизор";
+        searchText.sendKeys(tltvision);
+        driver.pressKey(new KeyEvent(AndroidKey.ENTER));
+        String foundTitleText = foundTitle.getText();
+        filterLabel.click();
+        filterSwitch.click();
+        applyFiltersButton.click();
+        String foundSaleTitleText = foundTitle.getText();
+        Assert.assertNotEquals(foundTitleText, foundSaleTitleText);
+        driver.lockDevice(Duration.ofSeconds(3));
+        Assert.assertEquals(television, searchText.getText());
+        
+        
     }
 
     @After
+    @Step("Закрытие драйвера")
     public void tearDown() {
         driver.quit();
     }
+    
+    Private enum Direction {
+        RIGHT,
+        LEFT,
+        UP,
+        DOWN
+        }
+        
+    @Step("Свайп {direction}")
+    private void swipe(Direction direction){
+        int border = 5;
+        int press = 100;
+        Dimension dims = driver.manage().window().getSize();
+        PointOption<?> pointOptionStart = PointOption.point(dims.width / 2, dims.height / 2);
+        PointOption<?> pointOptionEnd;
+        switch (direction) {
+            case RIGHT:
+                pointOptionEnd = PointOption.point(dims.width - border, dims.height / 2);
+                break;
+            case LEFT:
+                pointOptionEnd = PointOption.point(border, dims.height /2);
+                break;
+            case UP:
+                pointOptionEnd = PointOption.point(dims.width / 2, border);
+                break;
+            case DOWN:
+                pointOptionEnd = PointOption.point(dims.width / 2, dims.height - border);
+                break;
+            default:
+                throw new IllegalElementException("swipe is NOT supported");
+            
+        }
+        
+        new TouchAction<>(driver)
+            .press(pointOptionStart)
+            .waitAction(WaitOption.waitOptions(Duration.ofMillis(press)))
+            .moveTo(pointOptionEnd)
+            .release()
+            .perform();
+    }    
 }
